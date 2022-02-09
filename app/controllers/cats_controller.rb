f45f7f394adc6a243852d5aa19a44b49f56bf358
Mark Chuang <markchuangth@gmail.com>
2022-02-08 20:33:58 -0500

    inital commit

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,18 @@
+
+# EXPRESS JS
+# app.get('/urls', (req, res) => {
+   # const templateVars = { user: null };
+#    res.render('urls_index', templateVars);
+# })
+
+#  views folder 
+#  urls_index.ejs <--- 
+#  user
+# productRoutes.js
+# router.get('/products', (req, res) =>  {....})
+
+class CatsController < ApplicationController
+  def index
+
+  end
+end
